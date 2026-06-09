2784db169bfaf0edbf45662ddea64b9023ff7368
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2026-06-09 17:15:18 +0200

    Added default log dir name

--- calf/constants.h
+++ calf/constants.h
@@ -5,8 +5,12 @@
 #define _CALF_COMPONENT_NAME "calf"
 #endif
 
+#ifndef _CALF_DEFAULT_LOG_DIR_NAME
+#define _CALF_DEFAULT_LOG_DIR_NAME "./calf_logs"
+#endif
+
 constexpr unsigned int CALF_LOG_MAX_MSG_LEN           = 4096;
-constexpr char CALF_DEFAULT_LOG_FOLDER[]              = "./calf_logs";
+constexpr char CALF_DEFAULT_LOG_FOLDER[]              = _CALF_DEFAULT_LOG_DIR_NAME;
 constexpr char CALF_SYSCALL_DEFAULT_LOG_FILE_PREFIX[] = "syscall_";
 constexpr char CALF_STL_DEFAULT_LOG_FILE_PREFIX[]     = "stl_";
 constexpr char CALF_LOG_PRE_MSG[]                     = "%s";
